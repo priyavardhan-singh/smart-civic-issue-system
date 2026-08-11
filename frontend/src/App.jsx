5229fc1245530bada59b51ad0e1e9fc2084d902c
priyavardhan-singh <pintubanna8584@gmail.com>
2026-08-11 16:58:24 +0530

    Initial React project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="home">
+      <h1>Smart Civic Issue Reporting System</h1>
+
+      <p>
+        Report civic problems in your area and help make your city better.
+      </p>
+
+      <button>Report an Issue</button>
+    </div>
+  )
+} 
+
+export default App
